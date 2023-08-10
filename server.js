b11ecaad5467c42ab3797b92b97855cbe6138d9d
Dmiriy <kolesov.fsd@gmail.com>
2023-08-10 13:25:12 +0500

    fix

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
-const port = process.env.port || 3000;
-
 import {
     connectToColletion,
     getUsersData,
@@ -12,33 +9,51 @@ import {
     updateUserData,
 } from './mongo.js';
 
+const app = express();
+const port = process.env.port || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-app.get('https://test-node-server-be1d.onrender.com/users', async (req, res) => {
-    const usersCollection = await connectToColletion();
-    const usersData = await getUsersData(usersCollection);
-    res.json(usersData);
+app.get('/users/get', async (req, res) => {
+    try{
+        const usersCollection = await connectToColletion();
+        const usersData = await getUsersData(usersCollection);
+        res.json(usersData);
+    }catch(error){
+        console.log("Error GET users: " + error);
+    }
 });
 
-app.post('https://test-node-server-be1d.onrender.com/users/add', async(req, res) =>{
-    const usersCollection = await connectToColletion();
-    await addNewUser(usersCollection, req.body);
-    res.status(201).send('User added successfully');
+app.post('/users/add', async (req, res) => {
+    try{
+        const usersCollection = await connectToColletion();
+        await addNewUser(usersCollection, req.body);
+        res.status(201).send('User added successfully');
+    }catch(error){
+        console.log("Error ADD user: " + error);
+    }
 });
 
-app.delete('https://test-node-server-be1d.onrender.com/users/:id', async (req, res) => {
-    const usersCollection = await connectToColletion();
-    await deleteUser(usersCollection, req.params.id);
-    res.send('User deleted successfully');
+app.delete('/users/delete/:id', async (req, res) => {
+    try{
+        const usersCollection = await connectToColletion();
+        await deleteUser(usersCollection, req.params.id);
+        res.send('User deleted successfully');
+    }catch(error){
+        console.log("Error DELETE user: " + error);
+    }
 });
 
-app.put('https://test-node-server-be1d.onrender.com/users/update', async(req, res) => {
-    const usersCollection = await connectToColletion();
-    const updatedUser = req.body;
-
-    await updateUserData(usersCollection, updatedUser);
-    res.status(200).send('User data updated successfully')
+app.put('/users/update', async (req, res) => {
+    try{
+        const usersCollection = await connectToColletion();
+        const updatedUser = req.body;
+        await updateUserData(usersCollection, updatedUser);
+        res.status(200).send('User data updated successfully')
+    }catch(error){
+        console.log("Error UPDATE user: " + error);
+    }
 });
 
 app.listen(port, () => {
